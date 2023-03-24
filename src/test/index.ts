248f2fe90bc0a0d20685cf5b6d58c2a360e56a6b
zzdev <zzdev@developer.org>
2023-03-24 15:42:38 +0800

    :test_tube: Add test code

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,1 +1,11 @@
+import { PathController } from '@core/controller/PathController'
+import { matchScripts } from '@utils/matchScripts'
+import * as parserWxml from '@/lib/wxml-parser/src/parserWxml'
+
+const ctrl = PathController.make('files/watermark-test/watermark.html')
+const data = matchScripts(ctrl.readSync('utf8'))
+parserWxml(data, 'files/watermark-test').then((result) => {
+  JSON.stringify(result).length === 50462 && console.log('√ 改动不会影响结果')
+})
+
 export {}
