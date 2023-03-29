c072ade25a270b5e6c9e519528f4425a12892ace
zzdev <zzdev@developer.org>
2023-03-29 09:52:58 +0800

    :construction_worker:(workflow) "Add publish script"

---
+++ scripts/publish.js
@@ -0,0 +1,44 @@
+const { readFileSync, writeFileSync } = require('fs')
+const { resolve } = require('path')
+const { execSync } = require('child_process')
+require('colors')
+
+function log(message) {
+  console.log(`[LOG] ${message}`.blue)
+}
+
+function getDate() {
+  const d = new Date()
+  const to2 = (s) => ('00' + s).slice(-2)
+  return `${d.getFullYear()}-${to2(d.getMonth() + 1)}-${to2(d.getDate())}`
+}
+
+function isAlreadyRelease(log, version) {
+  const versionRE = `${version}`.replace(/[.-]/g, (r) => '\\' + r)
+  const d = new RegExp(`### \\[:bookmark:(v${versionRE})\\s*:loud_sound:`)
+  return d.exec(log) !== null
+}
+
+function main() {
+  // new version
+  execSync('npm version prerelease --preid=alpha --no-git-tag-version')
+  const { version } = require('../package.json')
+  const logPath = resolve(__dirname, '../CHANGELOG.md')
+  const changLog = readFileSync(logPath, 'utf8')
+  if (isAlreadyRelease(changLog, version)) return log(`v${version.bold} already released!`.red)
+  const newVersion = `# 更改日志
+
+### [:bookmark:v${version} :loud_sound:${getDate()}](https://github.com/r3x5ur/unveilr/tree/v${version})
+- 🐛解决部分已知问题
+---
+`
+  const newLog = newVersion + changLog.replace('# 更改日志\n', '')
+  writeFileSync(logPath, newLog)
+  execSync('git add ..')
+  execSync(`git commit -m ":bookmark:v${version}"`)
+  execSync(`git tag v${version}`)
+  execSync(`git push --tag -m ":bookmark:v${version}"`)
+  execSync(`npm publish`)
+  log(`v${version.bold} released!`.green)
+}
+main()
