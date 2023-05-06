b06c3ecf33bd1d4c39ffefe8d4532027f167f3ba
zzdev <zzdev@developer.org>
2023-05-06 10:55:01 +0800

    Optimize packaging results

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,25 @@
+import { join } from 'path'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
-import packages from './package.json'
-// import { terser } from 'rollup-plugin-terser'
-import conditional from 'rollup-plugin-conditional'
-// import alias from '@rollup/plugin-alias'
 import license from 'rollup-plugin-license'
-import { join } from 'path'
 import clear from 'rollup-plugin-clear'
-import babel from '@rollup/plugin-babel'
-import nodePolyfills from 'rollup-plugin-polyfill-node'
-const isBrowser = 1 || process.env.BUILD_MODE === 'browser'
-const outputNode = {
+import obfuscator from 'rollup-plugin-obfuscator'
+import { terser } from 'rollup-plugin-terser'
+import { dependencies } from './package.json'
+const output = {
   dir: 'dist',
   format: 'cjs',
   manualChunks(id) {
-    if (id.includes('wxml-parser-js')) return 'parser'
+    if (id.includes('wxml-parser')) return 'parser'
     if (id.includes('traverse.ts')) return 'traverse'
   },
 }
-const outputBrowser = {
-  dir: 'dist',
-  name: 'index',
-  format: 'umd',
-}
-const output = isBrowser ? outputBrowser : outputNode
-// const external = Object.keys(packages['dependencies'])
 export default {
   input: 'src/index.ts',
   output,
-  // external,
+  external: Object.keys(dependencies),
   plugins: [
     clear({
       targets: [output.dir],
@@ -44,21 +33,17 @@ export default {
       },
     }),
     commonjs(),
-    nodeResolve({ /* preferBuiltins: !isBrowser,*/ browser: isBrowser }),
-    babel({
-      babelHelpers: 'bundled',
-    }),
-    conditional(isBrowser, [
-      // nodePolyfills(),
-      // alias({
-      //   entries: {
-      //     path: 'path-browserify',
-      //   },
-      // }),
-    ]),
-
+    nodeResolve(),
     json(),
-    // terser(),
+    obfuscator({
+      options: {
+        compact: false,
+        splitStrings: true,
+        debugProtection: true,
+        controlFlowFlattening: true,
+      },
+    }),
+    terser(),
     license({
       banner: {
         commentStyle: 'ignored',
